70f116eb64e24f26e7314b0eeaf172f9d1833716
Muhammad Nabil Aisy <91885494+abilllll10@users.noreply.github.com>
2024-03-24 15:29:50 +0700

    Tugas 1

---
+++ Tugas 1/GAME-4604/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+using namespace std;
+
+class Character {
+public:
+    int numHerbs = 5;
+    int numManaPotions = 5;
+    int numHealthPotions = 5;
+    int swords[4] = {100, 200, 300, 400};
+
+    int currentSwordIndex = 0;
+    int healthPoints = 900;
+    int manaPoints = 900;
+    int damage = swords[currentSwordIndex];
+
+    void applyHerb() {
+        if (numHerbs < 1) {
+            cout << "You are out of herbs." << endl;
+        } else if (healthPoints >= 1000) {
+            cout << "You don't need to use herbs." << endl;
+        } else {
+            numHerbs--;
+            healthPoints += 10;
+            cout << "You applied a herb. +10 HP" << endl;
+        }
+    }
+
+    void applyHealthPotion() {
+        if (numHealthPotions < 1) {
+            cout << "You don't have any health potions left." << endl;
+        } else if (healthPoints >= 1000) {
+            cout << "You don't need to use health potions." << endl;
+        } else {
+            numHealthPotions--;
+            healthPoints += 50;
+            cout << "You used a health potion. +50 HP" << endl;
+        }
+    }
+
+    void applyManaPotion() {
+        if (numManaPotions < 1) {
+            cout << "You don't have any mana potions left." << endl;
+        } else if (manaPoints >= 1000) {
+            cout << "You don't need to use mana potions." << endl;
+        } else {
+            numManaPotions--;
+            manaPoints += 50;
+            cout << "You used a mana potion. +50 Mana" << endl;
+        }
+    }
+
+    void changeSword(int newIndex) {
+        if (newIndex >= 0 && newIndex < 3) {
+            currentSwordIndex = newIndex;
+            damage = swords[newIndex];
+            cout << "You have changed your sword." << endl;
+        } else {
+            cout << "Invalid sword index." << endl;
+        }
+    }
+
+    void displayStats() {
+        cout << "Health Points: " << healthPoints << endl;
+        cout << "Mana Points: " << manaPoints << endl;
+        cout << "Damage: " << damage << endl;
+        cout << "Current Sword Index: " << currentSwordIndex << endl << endl;
+    }
+
+    void displayInventory() {
+        cout << "+++ Inventory +++" << endl;
+        cout << "Herbs: " << numHerbs << endl;
+        cout << "Mana Potions: " << numManaPotions << endl;
+        cout << "Health Potions: " << numHealthPotions << endl << endl;
+    }
+};
+
+int main() {
+    Character player;
+    player.displayStats();
+    player.displayInventory();
+
+    player.applyHerb();
+    player.applyManaPotion();
+    player.applyHealthPotion();
+    player.changeSword(1);
+
+    cout << endl;
+
+    player.displayStats();
+    player.displayInventory();
+
+    return 0;
+}
